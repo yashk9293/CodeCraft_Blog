dd1bba8f7b6269c8a2395b6580b58c268eb72b93
Yash Kumar <90888884+yashk9293@users.noreply.github.com>
2025-04-03 00:41:38 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -25,6 +25,11 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the server!");
+});
+
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
 });
